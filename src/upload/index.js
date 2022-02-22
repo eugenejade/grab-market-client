a7bb565e9ad1e6526de3ea5c1216a9270f6bac10
eugene_jade <eugene_jade@dkitec.com>
2022-02-22 14:58:11 +0900

    test

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,5 +1,25 @@
+import { Divider, Form, Input } from "antd";
+import "./index.css";
 function UploadPage() {
-  return <h1>업로드 화면</h1>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <Form name="상품 업로드" onFinish={onSubmit}>
+        <Form.Item name="upload">
+          <div id="upload-img-placeholder">
+            <img src="/images/images/icons/camera.png" />
+            <span>이미지를 업로드해주세요</span>
+          </div>
+        </Form.Item>
+        <Divider />
+        <Form.Item label={<div className="upload-label">판매자 명</div>}>
+          <input className="upload-name" size="large" placeholder="이름을~" />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadPage;
