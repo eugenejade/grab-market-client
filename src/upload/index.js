1806516906d5b19669c7591e18498492e8dc12db
eugene_jade <eugene_jade@dkitec.com>
2022-02-22 15:47:14 +0900

    상품 업로드 폼 작업

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,4 +1,4 @@
-import { Divider, Form, Input } from "antd";
+import { Divider, Form, Input, InputNumber } from "antd";
 import "./index.css";
 function UploadPage() {
   const onSubmit = (values) => {
@@ -14,8 +14,46 @@ function UploadPage() {
           </div>
         </Form.Item>
         <Divider />
-        <Form.Item label={<div className="upload-label">판매자 명</div>}>
+        <Form.Item
+          label={<div className="upload-label">판매자 명</div>}
+          name="seller"
+          rules={[{ required: true, message: "판매자 이름을 입력해주세요" }]}
+        >
           <input className="upload-name" size="large" placeholder="이름을~" />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="name"
+          label={<div className="upload-label">상품 이름</div>}
+          rules={[{ required: true, message: "상품이름을 입력해주세요" }]}
+        >
+          <input
+            className="upload-name"
+            size="large"
+            placeholder="상품이름을 입력해주세요"
+          />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="price"
+          label={<div className="upload-label">상품가격</div>}
+          rules={[{ required: true, message: "상품가격을 입력해주세요" }]}
+        >
+          <InputNumber defaultValue={0} className="upload-price" size="large" />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="description"
+          label={<div className="upload-label">상품소개</div>}
+          rules={[{ required: true, message: "상품소개를 적어주세요" }]}
+        >
+          <Input.TextArea
+            size="large"
+            id="product-description"
+            showCount
+            maxLength={300}
+            placeholder="상품소개를 적어주세요"
+          />
         </Form.Item>
       </Form>
     </div>
